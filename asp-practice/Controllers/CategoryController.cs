db9e3a8fbe9e20049ecc3cf1a6daff3910646eac
nishaivy <113030466+nishaivy@users.noreply.github.com>
2023-01-03 15:08:01 +0530

    Navbar

---
+++ asp-practice/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using asp_practice.Data;
+using asp_practice.Models;
+using asppractice.Migrations;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Common;
+using Category = asp_practice.Models.Category;
+
+namespace asp_practice.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _context.Categories;
+            return View(categories);
+        }
+    }
+}
